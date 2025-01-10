ab2c27ef8ed3bb3815e677d5033a10609b17e95c
Ross Popovs <onyxiastolemylunch@gmail.com>
2025-01-10 17:27:51 +0000

    Trying aframe

---
+++ src/App-AFrame.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+    import "aframe"
+</script>
+
+<svelte:head>
+    <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script>
+</svelte:head>
+
+<a-scene artoolkit>
+
+    <a-assets>
+        <canvas id="myCanvas"></canvas>
+    </a-assets>
+
+    <a-marker type="pattern" url="../src/data/patterns/pattern-whale-marker.patt" size="0.15">
+        <a-box
+                scale="10 10 10"
+                position="0 0.5 0"
+                material="color: red;">
+        </a-box>
+    </a-marker>
+
+    <a-entity camera></a-entity>
+
+</a-scene>
--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,3 @@
-
 <script lang="ts">
     import {Canvas} from '@threlte/core'
     import {
@@ -12,106 +11,108 @@
         PlaneGeometry,
         Mesh, Clock, AmbientLight, BoxGeometry, PerspectiveCamera, Vector3, Vector4
     } from "three";
-    import { ArToolkitSource, ArToolkitContext, ArMarkerControls} from '@ar-js-org/ar.js/three.js/build/ar-threex.js';
+    import {THREEx, ARjs} from "@ar-js-org/ar.js-threejs"
     import {ARScene} from "./ARScene.js"
     import WhaleDescription from "./WhaleDescription.svelte";
 
-  //////////////////////////////////////////////////////////////////////////////////
-  //		Init
-  //////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////
+    //		Init
+    //////////////////////////////////////////////////////////////////////////////////
 
-  var renderer = new WebGLRenderer({
-      antialias: true,
-      alpha: true,
-      logarithmicDepthBuffer: false,
-      reverseDepthBuffer: false,
-  });
-  var width = 640; var height = 480;
+    var renderer = new WebGLRenderer({
+        antialias: true,
+        alpha: true,
+        logarithmicDepthBuffer: false,
+        reverseDepthBuffer: false,
+    });
+    var width = 640;
+    var height = 480;
 
-  let markerNotYetFound = true;
-  let markerFound = false;
+    let markerNotYetFound = true;
+    let markerFound = false;
 
-  renderer.setClearColor(new Color('lightgrey'), 0)
-  renderer.setSize( width, height );
-  renderer.domElement.style.position = 'absolute'
-  renderer.domElement.style.top = '0px'
-  renderer.domElement.style.left = '0px'
+    renderer.setClearColor(new Color('lightgrey'), 0)
+    renderer.setSize(width, height);
+    renderer.domElement.style.position = 'absolute'
+    renderer.domElement.style.top = '0px'
+    renderer.domElement.style.left = '0px'
 
-  document.body.appendChild( renderer.domElement );
+    document.body.appendChild(renderer.domElement);
 
-  // init scene and camera
-  var scene = new Scene();
+    // init scene and camera
+    var scene = new Scene();
 
-  //////////////////////////////////////////////////////////////////////////////////
-  //		Initialize a basic camera
-  //////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////
+    //		Initialize a basic camera
+    //////////////////////////////////////////////////////////////////////////////////
 
-  // Create a camera
-  var camera = new PerspectiveCamera(40, width / height, 1000, 10000);
-  camera.near = 10;
-  camera.far = 10000;
-  camera.updateProjectionMatrix();
-  scene.add(camera);
+    // Create a camera
+    var camera = new PerspectiveCamera(40, width / height, 1000, 10000);
+    camera.near = 10;
+    camera.far = 10000;
+    camera.updateProjectionMatrix();
+    scene.add(camera);
 
-  var light = new AmbientLight(Color.NAMES.white, 2.2);
-  scene.add(light);
+    var light = new AmbientLight(Color.NAMES.white, 2.2);
+    scene.add(light);
 
-  ////////////////////////////////////////////////////////////////////////////////
-  //          handle arToolkitSource
-  ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+    //          handle arToolkitSource
+    ////////////////////////////////////////////////////////////////////////////////
 
-  var arToolkitSource = new ArToolkitSource({
-      sourceType : 'webcam',
-      sourceWidth: 640,
-      sourceHeight: 480
-  })
+    var arToolkitSource = new THREEx.ArToolkitSource({
+        sourceType: 'webcam',
+        sourceWidth: 640,
+        sourceHeight: 480
+    })
 
-    function onResize(){
+    function onResize() {
         arToolkitSource.onResizeElement()
         arToolkitSource.copyElementSizeTo(renderer.domElement)
-        if( arToolkitContext.arController !== null ){
+        if (arToolkitContext.arController !== null) {
             arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas)
         }
     }
 
-  arToolkitSource.init(function onReady(){
-      // use a resize to fullscreen mobile devices
-      setTimeout(function() {
-          onResize()
-      }, 1000);
-  })
+    arToolkitSource.init(function onReady() {
+            // use a resize to fullscreen mobile devices
+            setTimeout(function () {
+                onResize()
+            }, 1000);
+        },
+        function onError() {
+            console.log("Error initialising arTooklitSource");
+        })
 
-  // handle resize
-  window.addEventListener('resize', function(){
-      onResize()
-  })
+    // handle resize
+    window.addEventListener('resize', function () {
+        onResize()
+    })
 
 
-  ////////////////////////////////////////////////////////////////////////////////
-  //          initialize arToolkitContext
-  ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+    //          initialize arToolkitContext
+    ////////////////////////////////////////////////////////////////////////////////
 
-  // create atToolkitContext
-  var arToolkitContext = new ArToolkitContext({
-      debug: true,
-      detectionMode: 'mono',
-      patternRatio: 0.85,
-      labelingMode: 'black_region',
-      canvasWidth: width
-  })
-    arToolkitContext.near = 100;
-    arToolkitContext.far = 10000;
+    THREEx.ArToolkitContext.baseURL = '@ar-js-org/ar.js-threejs';
+    var arToolkitContext = new THREEx.ArToolkitContext({
+        debug: false,
+        detectionMode: 'mono_and_matrix',
+        patternRatio: 0.85,
+        labelingMode: 'black_region',
+        canvasWidth: width,
+        cameraParametersUrl: "../src/data/camera_para.dat",
+    })
 
-  // initialize it
-  arToolkitContext.init(function onCompleted(){
-      // copy projection matrix to camera
-      camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix() );
-      camera.near = 10;
-      camera.far = 10000;
-      camera.updateProjectionMatrix();
-      arToolkitContext.arController.orientation = getSourceOrientation();
-      arToolkitContext.arController.options.orientation = getSourceOrientation();
-  })
+    arToolkitContext.init(function onCompleted() {
+        // copy projection matrix to camera
+        camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
+        camera.near = 10;
+        camera.far = 10000;
+        camera.updateProjectionMatrix();
+        arToolkitContext.arController.orientation = getSourceOrientation();
+        arToolkitContext.arController.options.orientation = getSourceOrientation();
+    })
 
 
     function getSourceOrientation() {
@@ -134,11 +135,11 @@
         }
     }
 
-  ////////////////////////////////////////////////////////////////////////////////
-  //          Create a ArMarkerControls
-  ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+    //          Create a ArMarkerControls
+    ////////////////////////////////////////////////////////////////////////////////
 
-  // init controls for camera
+    // init controls for camera
     var camTrackerRoot = new Group();
     scene.add(camTrackerRoot);
 
@@ -146,21 +147,21 @@
     scene.add(contentRoot);
 
 
-  var markerControls = new ArMarkerControls(arToolkitContext, camera, {
-      type : 'pattern',
-      size : 0.15,
-      patternUrl : "../src/data/patterns/pattern-whale-marker.patt",
-      changeMatrixMode: "cameraTransformMatrix",
-      //smooth: true,
-      //smoothCount: 30,
-      //smoothTolerance: 0.1,
-      //smoothThreshold: 15
-  })
+    var markerControls = new THREEx.ArMarkerControls(arToolkitContext, camera, {
+        type: 'pattern',
+        size: 0.15,
+        patternUrl: "../src/data/patterns/pattern-whale-marker.patt",
+        changeMatrixMode: "cameraTransformMatrix",
+        //smooth: true,
+        //smoothCount: 30,
+        //smoothTolerance: 0.1,
+        //smoothThreshold: 15
+    })
 
     //var markerLostNotice = document.getElementById("marker-lost-notice");
 
     markerControls.addEventListener("markerFound", () => {
-        if(markerFound == false){
+        if (markerFound == false) {
             markerNotYetFound = false;
             markerFound = true;
             console.log("Marker Found");
@@ -174,8 +175,9 @@
     })
 
     window.addEventListener("markerLost", () => {
-        if(markerFound == true){
+        if (markerFound == true) {
             markerFound = false;
+            console.log("Marker Lost");
             renderer.domElement.style.opacity = "0.4";
 
             //markerLostNotice.style.visibility = "visible";
@@ -183,48 +185,46 @@
     })
 
 
+    scene.visible = false;
 
-  scene.visible = false;
-
-  //////////////////////////////////////////////////////////////////////////////////
-  //		add an object in the scene
-  //////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////
+    //		add an object in the scene
+    //////////////////////////////////////////////////////////////////////////////////
 
     var arScene = new ARScene(renderer, camera);
     contentRoot.add(arScene.group);
 
 
-  update();
+    update();
 
-  function update()
-  {
+    function update() {
 
-      requestAnimationFrame(update);
+        requestAnimationFrame(update);
 
-      // if(camera.position.distanceTo(camTrackerRoot.position) < 1000) {
-      //     camera.position.lerp(camTrackerRoot.position, 0.15);
-      //     camera.quaternion.slerp(camTrackerRoot.quaternion, 0.15);
-      // }
+        // if(camera.position.distanceTo(camTrackerRoot.position) < 1000) {
+        //     camera.position.lerp(camTrackerRoot.position, 0.15);
+        //     camera.quaternion.slerp(camTrackerRoot.quaternion, 0.15);
+        // }
 
 
-      if ( arToolkitSource.ready !== false )
-          arToolkitContext.update( arToolkitSource.domElement );
+        if (arToolkitSource.ready !== false)
+            arToolkitContext.update(arToolkitSource.domElement);
 
-      scene.visible = true;
+        scene.visible = true;
 
-      render();
-  }
+        render();
+    }
 
-  function render(){
-      renderer.render( scene, camera );
-  }
+    function render() {
+        renderer.render(scene, camera);
+    }
 
 </script>
 
 <main>
-<!--    <div class="arjs-loader">-->
-<!--        <div class="arjs-loader-spinner"></div>-->
-<!--    </div>-->
+    <!--    <div class="arjs-loader">-->
+    <!--        <div class="arjs-loader-spinner"></div>-->
+    <!--    </div>-->
     {#if !markerFound}
         <div id="marker-lost-notice">
             {#if markerNotYetFound}
