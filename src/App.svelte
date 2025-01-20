041dd1425bffe008199e5f6dd8b42bf155c7d10a
Ross Popovs <onyxiastolemylunch@gmail.com>
2025-01-20 15:33:45 +0000

    Whale selector

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,12 @@
 <script>
-    import {Vector2} from "three";
+    import {Vector2, Vector3} from "three";
     import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
     import {Howl} from "howler";
 
     let appLaunched = $state(false);
     let selectedWhaleId = $state();
     let inWhaleSelection = $state(false);
+    let whaleSelector;
     console.log("App Scene");
     export const appInit = (latestScanPatternUrl) => {
         selectedWhaleId = latestScanPatternUrl;
@@ -13,6 +14,7 @@
         appLaunched = true;
         window.orbitControls.enabled = true;
         let globe = document.querySelector("#globe");
+        whaleSelector = document.querySelector("#whale-selector");
         globe.setAttribute("visible", true);
         AFRAME.ANIME({
             targets: [globe.object3D.position],
@@ -52,56 +54,69 @@
     const whaleIds = ["sperm-whale","blue-whale","humpback-whale"];
     var currentIndex = whaleIds.indexOf(selectedWhaleId);
     function launchWhaleSelectMenu(){
+        globe.object3D.scale.set(0,0,0);
         inWhaleSelection = true;
         console.log("Whale Selector");
-        // currentIndex = (currentIndex + 1 + whaleIds.length) % whaleIds.length;
-        // selectedWhaleId = whaleIds[currentIndex];
+        showSelectedWhale();
 
+        AFRAME.ANIME({
+            targets: [whaleSelector.object3D.scale],
+            x: 0.5,
+            y: 0.5,
+            z: 0.5,
+            easing: 'easeOutQuint',
+            duration: 750
+        });
+    }
+    function exitWhaleSelectMenu(){
+        whaleSelector.object3D.scale.set(0,0,0);
+        inWhaleSelection = false;
+
+        AFRAME.ANIME({
+            targets: [globe.object3D.scale],
+            x: 1,
+            y: 1,
+            z: 1,
+            easing: 'easeOutQuint',
+            duration: 750
+        });
+    }
+    function nextWhale(){
+        whaleSelector.object3D.scale.set(0,0,0);
+        currentIndex = (currentIndex + 1 + whaleIds.length) % whaleIds.length;
+        selectedWhaleId = whaleIds[currentIndex];
+        showSelectedWhale();
+        AFRAME.ANIME({
+            targets: [whaleSelector.object3D.scale],
+            x: 0.5,
+            y: 0.5,
+            z: 0.5,
+            easing: 'easeOutQuint',
+            duration: 500
+        });
+    }
+    function prevWhale(){
+        whaleSelector.object3D.scale.set(0,0,0);
+        currentIndex = (currentIndex - 1 + whaleIds.length) % whaleIds.length;
+        selectedWhaleId = whaleIds[currentIndex];
+        showSelectedWhale();
+        AFRAME.ANIME({
+            targets: [whaleSelector.object3D.scale],
+            x: 0.5,
+            y: 0.5,
+            z: 0.5,
+            easing: 'easeOutQuint',
+            duration: 500
+        });
+    }
+
+    function showSelectedWhale(){
         whaleIds.forEach((whaleId) => {
             document.getElementById(whaleId).setAttribute("visible", false);
             if(whaleId == selectedWhaleId){
                 document.getElementById(whaleId).setAttribute("visible", true);
             }
         })
-
-        let whaleSelector = document.querySelector("#whale-selector");
-
-        AFRAME.ANIME({
-            targets: [globe.object3D.scale],
-            x: 0,
-            y: 0,
-            z: 0,
-            easing: 'easeInBack',
-            duration: 750
-        });
-        AFRAME.ANIME({
-            targets: [whaleSelector.object3D.scale],
-            x: 0.5,
-            y: 0.5,
-            z: 0.5,
-            easing: 'easeInBack',
-            duration: 750
-        });
-    }
-    function exitWhaleSelectMenu(){
-        inWhaleSelection = false;
-        let whaleSelector = document.querySelector("#whale-selector");
-        AFRAME.ANIME({
-            targets: [globe.object3D.scale],
-            x: 1,
-            y: 1,
-            z: 1,
-            easing: 'easeInBack',
-            duration: 750
-        });
-        AFRAME.ANIME({
-            targets: [whaleSelector.object3D.scale],
-            x: 0,
-            y: 0,
-            z: 0,
-            easing: 'easeInBack',
-            duration: 750
-        });
     }
 </script>
 
@@ -150,17 +165,29 @@
 {#if appLaunched}
     <div class="whale-menu">
         {#if !inWhaleSelection}
-            <button class="whale-selector-button" on:click={launchWhaleSelectMenu}>
+            <button class="whale-selector-button" on:click={launchWhaleSelectMenu} aria-label="Enter Whale Selector">
                 <svg width="90" height="90">
                     <image xlink:href="../src/assets/whale-selector-button.svg" width="90" height="90"/>
                 </svg>
             </button>
         {:else}
-            <button class="whale-selector-button" on:click={exitWhaleSelectMenu}>
-                <svg width="90" height="90">
-                    <image xlink:href="../src/assets/globe-button.svg" width="90" height="90"/>
-                </svg>
-            </button>
+            <div style="display: flex; justify-content: center;">
+                <button class="whale-selector-left-button" on:click={prevWhale} aria-label="Previous Whale">
+                    <svg width="70" height="70">
+                        <image xlink:href="../src/assets/chevron-left-button.svg" width="70" height="70"/>
+                    </svg>
+                </button>
+                <button class="globe-button" on:click={exitWhaleSelectMenu} aria-label="Return to Globe">
+                    <svg width="90" height="90">
+                        <image xlink:href="../src/assets/globe-button.svg" width="90" height="90"/>
+                    </svg>
+                </button>
+                <button class="whale-selector-right-button" on:click={nextWhale} aria-label="Next Whale">
+                    <svg width="70" height="70">
+                        <image xlink:href="../src/assets/chevron-right-button.svg" width="70" height="70"/>
+                    </svg>
+                </button>
+            </div>
         {/if}
     </div>
 {/if}
